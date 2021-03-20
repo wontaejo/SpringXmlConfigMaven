b4821f5184d5a840e8c5b490ceb8b15013f7ceb9
wontaejo <wontaejo@wontaejoui-MacBookPro.local>
2021-03-20 23:54:10 +0900

    Test 를 위한 설정

---
+++ src/main/test/MySQLConnectionTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySQLConnectionTest {
+
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/study?useSSL=false&serverTimezone=Asia/Seoul";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    @Test
+    public void testConnection() throws Exception {
+        Class.forName(DRIVER);
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            System.out.println(connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
